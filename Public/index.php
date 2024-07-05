c984fff38e8736ceae5e78c744add4eeb8dcc081
SynnexSoftware <mujahidmjhd2001@gmail.com>
2024-07-05 21:20:05 +0530

    main

---
+++ Public/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php 
+    require '../views/head.php';
+    ?>
+</head>
+<body class="page-sign">
+
+    <div class="card card-sign">
+      <div class="card-header">
+        <a href="./" class="header-logo mb-4">Xiteb</a>
+        <h3 class="card-title">Sign In</h3>
+        <p class="card-text">Welcome back! Please signin to continue.</p>
+      </div><!-- card-header -->
+      <div class="card-body">
+        <form action="../handlers/login-handler.php">
+            <div class="mb-4">
+                <label class="form-label">Email address</label>
+                <input type="text" class="form-control" placeholder="Enter your email address">
+            </div>
+            <div class="mb-4">
+                <label class="form-label d-flex justify-content-between">Password <a href="forgot-password.php">Forgot password?</a></label>
+                <input type="password" class="form-control" placeholder="Enter your password">
+            </div>
+            <button type="submit" class="btn btn-primary btn-sign" name="sign-in">Sign In</button>
+        </form>
+      </div><!-- card-body -->
+      <div class="card-footer">
+        Don't have an account? <a href="sign-up.php">Create an Account</a>
+      </div><!-- card-footer -->
+    </div><!-- card -->
+
+  </body>
+</html>
---
+++ views/head.php
@@ -0,0 +1,7 @@
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xiteb | Practical Test</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../Assets/css/style.css">
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <script src="../jquery/jquery.js"></script>
