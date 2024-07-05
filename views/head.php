b3bb2f0aeb3c5211a61bb1fdb9cd377b31c29cbe
SynnexSoftware <mujahidmjhd2001@gmail.com>
2024-07-06 02:18:07 +0530

    dashboard

--- views/head.php
+++ views/head.php
@@ -3,8 +3,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Xiteb | Practical Test</title>
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../fontawesome/css/all.css">
     <link rel="stylesheet" href="../Assets/css/style.css">
+    <link rel="stylesheet" href="../Assets/css/sidebar.css">
     <script src="../bootstrap/js/bootstrap.min.js"></script>
     <script src="../jquery/jquery.js"></script>
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
